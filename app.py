ff8051337d814895d6f1e6665e4e21be217c1d3f
Shivam Singh <sivamsinghsh@gmail.com>
2023-01-09 23:55:16 +0530

    Use Streamlit to create a personal website

---
+++ app.py
@@ -0,0 +1,109 @@
+import streamlit as st
+from PIL import Image
+import requests
+from streamlit_lottie import st_lottie
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+# ---- LOAD ASSETS ----
+lottie_coding = load_lottieurl("https://assets3.lottiefiles.com/packages/lf20_o6spyjnc.json")
+img_contact_form = Image.open("images/sample.jpg")
+img_lottie_animation = Image.open("images/base.gif")
+
+# -----------Header section------------
+
+with st.container():
+	st.subheader("Hi, I am Shivam Singh :wave:")
+	st.title("A Data Analyst From India")
+	st.write("Being an ambitious individual, I strongly believe in learning new technologies and analytical skills to conclude to optimized business solutions. I also believe in prioritizing understanding of the data model to get the best analytical view of data.")
+	st.write("[Learn More >](www.sivamsinghsh.io)")
+
+# ------------What I DO------------------
+with st.container():
+	st.write("---")
+	left_column, right_column = st.columns(2)
+	with left_column:
+		st.header("What I do")
+
+		st.write(
+            """
+            On my YouTube channel I am creating tutorials for people who:
+            - are looking for a way to leverage the power of Python in their day-to-day work.
+            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
+            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
+            - are working with Excel and found themselves thinking - "there has to be a better way."
+            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
+            """
+        )
+        # st.write("[YouTube Channel >](https://youtube.com)")
+with right_column:
+   	st_lottie(lottie_coding, height=300, key="coding")
+
+
+
+
+# ----------------------------
+with st.container():
+    st.write("---")
+    st.header("My Projects")
+    st.write("##")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(img_lottie_animation)
+    with text_column:
+        st.subheader("Integrate Lottie Animations Inside Your Streamlit App")
+        st.write(
+            """
+            Learn how to use Lottie Files in Streamlit!
+            Animations make our web app more engaging and fun, and Lottie Files are the easiest way to do it!
+            In this tutorial, I'll show you exactly how to do it
+            """
+        )
+        st.markdown("[Watch Video...](https://youtu.be)")
+with st.container():
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(img_contact_form)
+    with text_column:
+        st.subheader("How To Add A Contact Form To Your Streamlit App")
+        st.write(
+            """
+            Want to add a contact form to your Streamlit website?
+            In this video, I'm going to show you how to implement a contact form in your Streamlit app using the free service ‘Form Submit’.
+            """
+        )
+        st.markdown("[Watch Video...](https://youtu.be)")
+
+# ---- CONTACT ----
+with st.container():
+    st.write("---")
+    st.header("Get In Touch With Me!")
+    st.write("##")
+
+    # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
+    contact_form = """
+    <form action="https://formsubmit.co/sivamsinghsh@gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
